384c07b6f55327d7bd31df04e19f6a9a8c354bdb
Bartosz Grabowski <58475557+bartosz-grabowski@users.noreply.github.com>
2025-02-17 21:01:49 +0100

    Add demo script

---
+++ scripts/demo.py
@@ -0,0 +1,15 @@
+# Copyright (c) 2025 bartosz-grabowski
+
+from basicmath.ops.addition import add
+from basicmath.ops.subtraction import subtract
+
+
+def main():
+    a = 3
+    b = 1
+    print(f'Sum of a={a} and b={b} is equal to {add(a, b)}')
+    print(f'Difference of a={a} and b={b} is equal to {subtract(a, b)}')
+
+
+if __name__ == "__main__":
+    main()
