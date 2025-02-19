7e7fa250ba512d66fbc0a6e8fa4ffebf4d10f8be
Bartosz Grabowski <58475557+bartosz-grabowski@users.noreply.github.com>
2025-02-19 23:56:24 +0100

    Add unit tests for add and subtract

---
+++ tests/ops/subtraction/test_subtract.py
@@ -0,0 +1,18 @@
+# Copyright (c) 2025 bartosz-grabowski
+
+import pytest
+
+from basicmath.ops.subtraction import subtract
+
+
+def test_subtract_on_numbers():
+    assert subtract(2, 3) == -1
+
+
+def test_subtract_on_string():
+    with pytest.raises(TypeError):
+        subtract('a', 1)
+
+def test_subtract_on_strings():
+    with pytest.raises(TypeError):
+        subtract('a', 'tt')
